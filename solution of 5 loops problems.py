b53fe5e61ba5b1d82085ad6b7294b0bfc0645d32
Pratyasha430 <dpratyasha2007@gmail.com>
2026-09-09 23:04:05 +0530

    Add files via upload

---
+++ solution of 5 loops problems.py
@@ -0,0 +1,56 @@
+# Question no. 1-->
+
+# number = [1, -2, 3, -4, 5, 6, -7, -8, 9, 10]
+# positive_number_count = 0
+
+# for num in number:
+#  if num> 0:
+#   positive_number_count +=1
+
+# print("Final count of positine number is: ", positive_number_count)
+
+
+# Question no. 2-->
+
+# n = int(input("Enter your number: "))
+# sum_even = 0
+
+# for i in range(1, n):
+#  if i % 2 == 0:
+#   sum_even += 1
+
+# print("Sum of even number is: ", sum_even)
+
+
+# Question no. 3-->
+
+# num = int(input("Enter your table number: "))
+
+# for i in range(1, 11):
+#  if i == 5:
+#    continue
+#  print(num, 'x', i, '=', num * i)
+ 
+
+# Question no. 4-->
+
+# str = input("Enter your string: ")
+# reversed_str = ""
+
+# for char in str:
+#  reversed_str  = char + reversed_str
+
+# print(reversed_str)
+
+
+# Question no. 5-->
+
+# str = "Pratyasha"
+
+# for char in str:
+#  print(char)
+#  if str.count(char) == 1:
+#   print("Char is: ",char)
+#   break
+
+
